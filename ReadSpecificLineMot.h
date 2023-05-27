ba9cb67af74321c2684f9f600b84a089c159e2e1
Paul H <133357198+PaulHpsr@users.noreply.github.com>
2023-05-27 23:09:53 +0200

    Update ReadSpecificLineMot.h

--- ReadSpecificLineMot.h
+++ ReadSpecificLineMot.h
@@ -4,28 +4,31 @@
 #define FILENAME_SIZE 1024
 #define MAX_LINE 30
 
-int len;
 const char* ReadLine(int read_line, char nomfile[])
 {
+  int len;
   FILE *file;
   char buffer[30];
 char *buff;
+  //Verif si le fichier existe
   file = fopen(nomfile,"r");
 if(file ==NULL){
   printf("Erreur\n");
 }  
+  
 bool keep_reading = true;
 int current_line =1;
+  
   do
     {
       fgets(buffer, MAX_LINE, file);
-      if(feof(file)){
-        keep_reading = false;
+      if(feof(file)){                //Si on arriv eà la fin du fichier
+        keep_reading = false;        // On arrête de lire
       }
-      else if(current_line==read_line){
+      else if(current_line==read_line){    //Si on est sur la ligne voulue, on arrête de lire + on retourne ce qu'il y a sur la ligne
         keep_reading = false;
         len = strlen(buffer);
-         if (buffer[len-1] == '\n')
+         if (buffer[len-1] == '\n')    //Lorsque on utilise fgets, il y a un \n qui s'ajoute en + , du coup ici on l'enlève car on aura besoin parfois de comparer (strcpm) 
          buffer[len-1] = 0;
         buff = buffer;
         return buff;
